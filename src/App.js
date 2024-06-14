e9dbe9a674b53960751630c82e4e56ee69084ed4
Valerii <badday182@gmail.com>
2024-06-14 16:45:06 +0300

    add some style

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ function App() {
   const [hidden, setHidden] = useState("");
   const [hidden2, setHidden2] = useState("hidden");
   const [hidden3, setHidden3] = useState("hidden");
-  const [hidden4, setHidden4] = useState("hidden"); 
+  const [hidden4, setHidden4] = useState("hidden");
 
   const handlePlayOnClick = () => {
     setPlay(true);
     // setHidden("hidden");
   };
-
 
   const hahdleEnded = () => {
     setPlay2(true);
@@ -27,30 +26,43 @@ function App() {
         <div className={`${hidden} absolute top-0 left-0`}>
           <ReactPlayer
             url={process.env.PUBLIC_URL + "./video/start.mp4"}
+            // url={"https://youtu.be/XTAX2QTjEpc"}
             // url={url}
             playing={play}
-            muted={false}
             volume={0.8}
             onEnded={hahdleEnded}
+            // onDuration
             // loop={loop}
             // controls
+            // config={{
+            //   youtube: {
+            //     playerVars: { controls: 0},
+            //   },
+            // }}
           />
         </div>
 
         <div className={`${hidden2} absolute top-0 left-0`}>
           <ReactPlayer
             url={process.env.PUBLIC_URL + "./video/choise waiting.mp4"}
+            // url={"https://youtu.be/ECE460i4_ZQ"}
             playing={play2}
             muted={false}
             volume={0.8}
             // onEnded={switchUrl}
             loop={true}
             // controls
+            // config={{
+            //   youtube: {
+            //     playerVars: { controls: 0, autoplay: 1},
+            //   },
+            // }}
           />
         </div>
         <div className={`${hidden3} absolute top-0 left-0`}>
           <ReactPlayer
             url={process.env.PUBLIC_URL + "./video/help.mp4"}
+            // url={"https://youtu.be/ImJm5skGtOI"}
             playing={play3}
             muted={false}
             volume={0.8}
@@ -62,6 +74,7 @@ function App() {
         </div>
         <div className={`${hidden4} absolute top-0 left-0`}>
           <ReactPlayer
+            // url={"https://youtu.be/r0FgNebMhvU"}
             url={process.env.PUBLIC_URL + "./video/money.mp4"}
             playing={play4}
             muted={false}
@@ -72,12 +85,8 @@ function App() {
             // controls
           />
         </div>
-      </div>
-      <div className="">
-        <button className="border-2 border-black" onClick={handlePlayOnClick}>
-          Play
-        </button>
-        {play2 && (
+        <div className='absolute bottom-9 left-16 bg-white/50 '>
+         {play2 && (
           <>
             <button
               className="border-2 border-black"
@@ -93,7 +102,9 @@ function App() {
             >
               Choise 1 Help
             </button>
-            <button className="border-2 border-black"  onClick={() => {
+            <button
+              className="border-2 border-black"
+              onClick={() => {
                 // setChoise1(
                 //   "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"
                 // );
@@ -101,7 +112,8 @@ function App() {
                 setHidden4("");
                 setPlay2(false);
                 setPlay4(true);
-              }}>
+              }}
+            >
               Choise 2 Leave
             </button>
             {/* <button className="border-2 border-black" onClick={() => {}}>
@@ -109,6 +121,13 @@ function App() {
             </button> */}
           </>
         )}
+        </div>
+      </div>
+      <div className="">
+        <button className="border-2 border-black" onClick={handlePlayOnClick}>
+          Play
+        </button>
+       
       </div>
     </>
   );
